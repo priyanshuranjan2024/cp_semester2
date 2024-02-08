1b73856a32c7fb1b0fab9243fb3b05f186882989
Priyanshu Ranjan <133768704+priyanshuranjan2024@users.noreply.github.com>
2024-02-08 17:26:41 +0530

    Update p6_2.c

--- p6_2.c
+++ p6_2.c
@@ -3,8 +3,11 @@
 int main(){
     char a;
     scanf("%s",&a);
-    if(a=='a' || a=='e' ||a=='i' ||a=='o' ||a=='u') printf("Vowel");
-    else printf("Consonant");
+    if(a>='a' && a<='z' ||a>='A' && a<='Z'){
+        if(a=='a' || a=='e' ||a=='i' ||a=='o' ||a=='u'||a=='A'||a=='E'||a=='I' || a=='O'||a=='U') printf("Vowel\n");
+        else printf("Consonant\n");
+    }
+    else printf("the entered character is not an alphabet\n");
 
 
     return 0;
